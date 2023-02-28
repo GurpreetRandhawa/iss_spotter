c718ea1261f5ca93d6ef5c6d07ad90d32e959522
GurpreetRandhawa <singhpreet01991@gmail.com>
2023-02-28 15:21:09 -0800

    fetch geo cordinates fucntion added

---
+++ index.js
@@ -0,0 +1,17 @@
+// const { fetchMyIP, fetchCoordsByIP } = require("./iss");
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It did not work!", error);
+//     return;
+//   }
+//   console.log("It worked! Returned IP: ", ip);
+// });
+
+// fetchCoordsByIP("207.216.253.1kh99", (error, coor) => {
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+//   console.log(coor);
+// });
